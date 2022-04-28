f3145dfdd5ae430ee703b286b802048fecbf4516
VadzimSudaranka <104575948+VadzimSudaranka@users.noreply.github.com>
2022-04-28 17:24:43 +0300

    Add files via upload

---
+++ JavaAcademyApp.java
@@ -0,0 +1,19 @@
+public class JavaAcademyApp {
+    public static void main(String[] args) {
+        String argument = "JavaAcademy";
+            int[] symbols = new int[26];
+            for (char c : argument.toCharArray()) {
+                if ((c >= 'a') && (c <= 'z')) {
+                    symbols[c - 'a']++;
+                } else if ((c >= 'A') && (c <= 'Z')) {
+                    symbols[c - 'A']++;
+
+            }
+            for (int i = 0; i < 26; i++){
+                if (symbols[i] != 0){
+                    System.out.println((char) ('a' + i) + ": " + symbols[i]);
+                }
+            }
+        }
+    }}
+
